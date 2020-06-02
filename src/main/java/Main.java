18e362d9557ec1c8f02e24d3baa3651638f0fc9a
Jakub W <jakub.wolnyy@gmail.com>
2020-06-02 20:56:09 +0200

    Stage 3 implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,41 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Main {
+public class Main {
+
     public static void main(String[] args) {
+        run(peopleInput());
+    }
+
+    private static void run(String[] listOfPeople){
+
+        System.out.println("=== Menu ===");
+        System.out.println("1. Find a person");
+        System.out.println("2. Print all people");
+        System.out.println("0. Exit");
+        Scanner sc = new Scanner(System.in);
+        String command = sc.next();
+
+        switch (command){
+            case "1":
+                search(listOfPeople);
+                run(listOfPeople);
+                break;
+            case "2":
+                print(listOfPeople);
+                run(listOfPeople);
+                break;
+            case "0":
+                System.out.println("Bye!");
+                break;
+            default:
+                System.out.println("Incorrect option! Try again.");
+                run(listOfPeople);
+        }
+    }
+
+    private static String[] peopleInput() {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of people:");
         int numberOfLines = sc.nextInt();
@@ -13,29 +46,36 @@ class Main {
         for (int i = 0; i < people.length; i++) {
             people[i] = sc.nextLine();
         }
-        System.out.println("Enter the number of search queries:");
-        int numberOfSearchQueries = sc.nextInt();
+        return people;
+    }
 
-
-        for (int j = 0; j <numberOfSearchQueries ; j++) {
-            System.out.println("Enter data to search people:");
-            String keyWorld = sc.next();
-            List<Integer> indexes = new ArrayList<>();
-            for (int k = 0; k < people.length ; k++) {
-                if (people[k].toLowerCase().contains(keyWorld.toLowerCase())){
-                    indexes.add(k);
-                }
-            }
-
-            if (!indexes.isEmpty()){
-                System.out.println("Found people:");
-                for (int x: indexes) {
-                    System.out.println(people[x]);
-                }
-            }
-            else {
-                System.out.println("No matching people found.");
+    private static void search(String[] listOfPeople){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a name or email to search all suitable people:");
+        String keyWorld = sc.next();
+        List<Integer> indexes = new ArrayList<>();
+        for (int k = 0; k < listOfPeople.length; k++) {
+            if (listOfPeople[k].toLowerCase().contains(keyWorld.toLowerCase())) {
+                indexes.add(k);
             }
         }
+
+        if (!indexes.isEmpty()) {
+            System.out.println("Found people:");
+            for (int x : indexes) {
+                System.out.println(listOfPeople[x]);
+            }
+        } else {
+            System.out.println("No matching people found.");
+        }
+
+    }
+
+    public static void print(String[] listOfPeople){
+        System.out.println("=== List of people ===");
+        for (String person: listOfPeople) {
+            System.out.println(person);
+        }
+
     }
 }
